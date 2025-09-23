ec1e34a0c67117d37162cec89cb944a3efc55765
Toth Alexandra <63046754+Alexandra7a@users.noreply.github.com>
2025-09-23 13:40:21 +0300

    m2

--- lxc/main.tf
+++ lxc/main.tf
@@ -17,3 +17,22 @@ resource "null_resource" "postgres_setup" {
     }
   }
 }
+resource "null_resource" "postgres_setup" {
+  depends_on = [proxmox_lxc.pve_lxc]
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update",
+      "apt-get install -y postgresql",
+      "sudo -u postgres psql -c "ALTER USER postgres PASSWORD '\{var.postgres_password}';"",
+      "sudo -u postgres createdb \${var.postgres_db_name}"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = proxmox_lxc.pve_lxc.ip
+      user        = "postgres"
+      password    = var.root_pass
+    }
+  }
+}
--- lxc/variables.tf
+++ lxc/variables.tf
@@ -11,3 +11,16 @@ variable "postgres_db_name" {
   default     = "mydatabase"
   nullable    = false
 }
+variable "postgres_password" {
+  type        = string
+  sensitive   = true
+  description = "The password for the PostgreSQL user"
+  nullable    = false
+}
+
+variable "postgres_db_name" {
+  type        = string
+  description = "The name of the PostgreSQL database to create"
+  default     = "mydatabase"
+  nullable    = false
+}
